8efd5b723d7c586b4320ae8f718f12706679368e
Lara Hoya <larascully@hotmail.com>
2015-12-08 07:36:21 +0100

    travels show template

--- app/assets/javascripts/travels.js
+++ app/assets/javascripts/travels.js
@@ -1,2 +1,38 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
+(function() {
+
+'use strict';
+var ajax;
+
+TravelApp.Travel = function (id) {
+  this.id = id;
+  ajax = new TravelApp.Ajax();
+}; 
+
+TravelApp.Travel.prototype.show = function() {
+  ajax.get('/travels/' + this.id, this.printInfo);
+}
+
+TravelApp.Travel.prototype.printInfo = function(travel) {
+  $('#content').html(HandlebarsTemplates['travels/show'](travel));
+}
+
+})()
+
+$(document).on('ready', function() {
+
+  $('.link-travel').on('click', function(event) {
+    event.preventDefault();
+    var $link = $(event.currentTarget);
+    var id = $link.data('id');
+    $('#content').empty();
+
+    var travel = new TravelApp.Travel(id);
+    travel.show();
+  })
+
+  $('.link-travel-new').on('click', function(event) {
+    event.preventDefault();
+    $('#content').empty();
+    $('#content').html(HandlebarsTemplates['travels/new']);
+  })
+})
